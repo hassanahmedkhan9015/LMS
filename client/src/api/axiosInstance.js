f06c68da4ead3256ecc9d269827225869a62e12b
hassanahmedkhan9015 <hassankhan108017@gmail.com>
2024-11-24 15:26:25 +0500

    feaat: update

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
-  // baseURL: "https://lmstesting.vercel.app/"
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://lmstesting.vercel.app/"
 });
  
 axiosInstance.interceptors.request.use(
